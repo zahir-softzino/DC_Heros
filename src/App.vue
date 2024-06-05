684fc767fdab3a1e1f881ee96f4c03e74be7bc9b
zahir-softzino <zahir.softzino@gmail.com>
2024-06-05 09:25:10 +0600

    init first

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="w-full flex">
+    <div class="m-auto">
+      <AppHeader />
+      <dcHeros />
+    <!-- </div>
+    <div class="m-auto"> -->
+      <!-- <ul>
+        <li class="flex justify-between" v-for="(hero, index) in dcHeros" :key="index">
+          <div>
+            {{ hero.name }}
+          </div>
+          <button @click="remove(index)">x</button>
+        </li>
+      </ul> -->
+
+      <form class="mt-10" @submit.prevent="addHero">
+        <input class="border border-gray-800 rounded mr-5" v-model.lazy="newHero" placeholder="New Hero"/>
+        <button class="border rounded bg-gradient-to-r from-gray-200 to-gray-500" type="submit">Add hero</button>  
+      </form>
+    </div>    
+    
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+ 
+  import AppHeader from "./components/AppHeader.vue"
+  import dcHeros from "./components/dcHeros.vue"
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {AppHeader, dcHeros},
+  
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="w-full bg-gradient-to-r from-blue-800 to-blue-600 text-white py-4 text-center text-2xl">
+    <a href="#">DC Heros</a>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
